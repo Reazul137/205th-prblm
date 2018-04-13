85c613f76f9c4e3ae09da68a4e45dd3acb3a2807
Reazul Islam <reazulislam137@gmail.com>
2018-04-14 01:30:17 +0600

    first commit

---
+++ add dunction.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2, sum;
+    printf("Enter the two values : ");
+    scanf("%d %d", &num1,&num2);
+
+    printf("sum = %d",sum);
+
+}
+int add (int a, int b)
+{
+    int add;
+    add = a+b;
+    return add;
+}
